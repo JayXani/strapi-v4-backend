6f4a3390905c395ec7505a36a4bb7290b8d90d7b
JayXani <112290478+JayXani@users.noreply.github.com>
2024-02-28 12:34:38 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ config/middlewares.js
@@ -0,0 +1,33 @@
+module.exports = [
+  'strapi::logger',
+  'strapi::errors',
+  {
+    //Objeto que realiza a configuração do cloudinary nos middlewares, quando tiver
+    //um upload ele vai interceptar a imagem e enviar para o cloudnary
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'market-assets.strapi.io',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
